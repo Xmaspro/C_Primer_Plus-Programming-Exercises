ced1d4a67a0287f18cea72d3ee99b60bb76fb67a
Xmaspro <61029176+Xmaspro@users.noreply.github.com>
2022-06-10 16:52:18 +0800

    Create 5-5.c

---
+++ Chapter 5/5-5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)                /* finds sum of first 20 integers */
+{
+
+    int days,count, sum;           /* declaration statement          */
+    printf("Enter how many days: \n");
+    scanf("%d", &days);
+    count = 0;                /* assignment statement           */
+    sum = 0;                  /* ditto                          */
+
+    while (count++ < days)      /* while                          */
+        sum = sum + count;    /*     statement                  */
+    printf("sum = %d\n", sum);/* function statement             */
+  
+    return 0;
+}
