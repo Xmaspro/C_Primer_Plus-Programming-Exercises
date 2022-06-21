96b8d7469d123e8e873e21e75e4af5bdcde58ce3
Xmaspro <61029176+Xmaspro@users.noreply.github.com>
2022-06-21 20:49:14 +0800

    Create 11-3.c
    have bugs.

---
+++ Chapter 11/11-3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#define SIZE 30
+char *get_word(char *out);
+
+int main(int argc, char *argv[])
+{
+    char output[SIZE];
+    get_word(output);
+    puts("The string is:");
+    puts(output);
+
+    return 0;
+}
+
+char *get_word(char *out)
+{
+    char input[SIZE];
+    char *in = input;
+    puts("Enter texts:");
+    fgets(input, SIZE, stdin);
+
+    while (*in == ' ' || *in == '\n'|| *in == '\t'||*in =='\0')
+        in++;
+    while (*in != ' ' || *in != '\n'|| *in != '\t'||*in =='\0')
+        *out++ = *in++;
+
+    return out;
+}
